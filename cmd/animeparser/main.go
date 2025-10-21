299a4989b8086029cb1a5234d86b95f7e172cb6c
Hantan <tease@eteb.me>
2025-10-21 15:28:08 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/animeparser/main.go
@@ -0,0 +1,23 @@
+// cmd/animeparser/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "animeparser/internal/animeparser"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := animeparser.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
